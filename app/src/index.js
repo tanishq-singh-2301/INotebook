faab1c616f328d7deae9d80d3a49e3faaf0d84fd
Tanishq Singh <>
2021-09-08 13:35:25 +0530

    beta-test v0.0.1

--- app/src/index.js
+++ app/src/index.js
@@ -1,21 +1,17 @@
 import React, { useContext } from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import Home from './views/Home';
-import About from './views/About';
-import Navbar from './includes/Navbar';
-import AddNote from './views/AddNote';
-import AllNotes from './views/AllNotes';
-import Login from './views/Login';
-import Register from './views/Register';
-// import WebsiteHomePage from './views/WebsiteHomePage';
-// import NotFound from './views/NotFound';
+import Home from 'src/views/Home';
+import About from 'src/views/About';
+import Navbar from 'src/includes/Navbar';
+import AddNote from 'src/views/AddNote';
+import AllNotes from 'src/views/AllNotes';
+import Login from 'src/views/Login';
+import Register from 'src/views/Register';
 import NavbarContext from 'src/context/navbar/navbarContext';
 import NavbarState from 'src/context/navbar/navbarState';
 import NotesState from 'src/context/notes/State';
-// import AuthContext from 'src/context/auth/Context';
-// import AuthState from 'src/context/auth/State';
-import './index.css';
+import 'src/index.css';
 import { FaHome, BiNotepad, IoCalendar } from 'react-icons/all'
 
 const App = () => {
