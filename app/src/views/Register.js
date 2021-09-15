6f49874d9e2e6428b0f4c71f1dadf8c6c593197e
Tanishq Singh <>
2021-09-16 01:08:03 +0530

    minor bug fix

--- app/src/views/Register.js
+++ app/src/views/Register.js
@@ -44,7 +44,7 @@ const Register = () => {
             <section style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', height: '100vh', width: '100vw', flexDirection: 'column', background: 'linear-gradient(to left, #bdc3c7, #2c3e50)', border: 'none' }}>
                 <div style={{ top: '0', left: '0', position: 'fixed', alignItems: 'center', justifyContent: 'space-between', display: 'flex', padding: '0 30px', height: '10vh', width: '100%' }}>
                     <span style={{ textDecoration: 'none', color: 'white', fontSize: 'larger', fontWeight: '600' }}>INotebook</span>
-                    <Link to="/register" style={{ textDecoration: 'none', color: '#fff', fontSize: 'large', fontWeight: '600', textShadow: '1px 1px 50px #000', borderBottom: '2px solid whitesmoke' }}>Register</Link>
+                    <Link to="/login" style={{ textDecoration: 'none', color: '#fff', fontSize: 'large', fontWeight: '600', textShadow: '1px 1px 50px #000', borderBottom: '2px solid whitesmoke' }}>Login</Link>
                 </div>
                 <form onSubmit={submitForm} className='login__form'>
                     <input required type='text' value={cred.name} onChange={(e) => setCred({ ...cred, 'name': e.target.value })} minLength={5} autoComplete="off" className='login__form__input' placeholder="Username" />
@@ -53,10 +53,8 @@ const Register = () => {
 
                     {
                         isLoading ?
-                            <div className="spinner-border spinner-border-sm" style={{ backgroundColor: 'transparent', color: 'whitesmoke', fontSize: 'medium', fontWeight: '500' }} role="status">
-                                <span className="visually-hidden">Loading...</span>
-                            </div> :
-                            <button type='submit' className="login__form__submit__btn">Login</button>
+                            <div className="spinner-border spinner-border-sm" style={{ backgroundColor: 'transparent', color: 'whitesmoke', fontSize: 'medium', fontWeight: '500' }} role="status"></div>
+                            : <button type='submit' className="login__form__submit__btn">Register</button>
                     }
                 </form>
             </section>
